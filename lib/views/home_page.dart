590740d1fbfb75a917d905d81983879ba45a768b
lucas-rss <52760079+lucas-rss@users.noreply.github.com>
2020-09-23 19:51:34 -0300

    Update home_page.dart
    Configuração estados no widget padrão.

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -13,10 +13,39 @@ class _HomePageState extends State<HomePage>{
     viewportFraction: .2,
   );
   
-  @override
+ @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Color(),
+      backgroundColor: Color(#4d4d4d),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: BlocBuilder<MovieBloc, MovieState>(
+          builder: (context, state) {
+            if (state is MovieInitial) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (state is MovieFailed) {
+              return Center(
+                child: Text('Houve um problema...'),
+              );
+            }
+
+            if (state is MovieSuccess) {
+              if (state.movies.isEmpty) {
+                return Center(
+                  child: Text('Sem Filmes'),
+                );
+              }
+
+              return _buildMovies(state);
+            }
+            return Center(child: CircularProgressIndicator());
+          },
+        ),
+      ),
     );
   }
-}
