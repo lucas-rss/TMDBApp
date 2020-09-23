fb7a82d5510799358c7a8782cc24669a48209685
lucas-rss <52760079+lucas-rss@users.noreply.github.com>
2020-09-23 10:48:10 -0300

    Update movies_bloc.dart

--- lib/bloc/movies_bloc.dart
+++ lib/bloc/movies_bloc.dart
@@ -8,3 +8,25 @@ abstract class MovieEvent extends Equatable {
 }
 
 class FetchMovies extends MovieEvent{}
+
+//ESTADOS
+
+abstract class MovieState extends Equatable{
+  const MovieState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class MovieInitial extends MovieState{}
+class MovieFailed extends MovieState{}
+class MovieSuccess extends MovieState{
+  final List<Movie> movies;
+  final bool hasReachedMax;
+
+  MovieSuccess({
+
+    this.movies,
+    this.hasReachedMax  
+  });
+}
