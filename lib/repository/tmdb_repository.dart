8b84d21dfcf05758937b6e2b05603eeae3a854e1
lucas-rss <52760079+lucas-rss@users.noreply.github.com>
2020-09-23 10:10:59 -0300

    Add files via upload

---
+++ lib/repository/tmdb_repository.dart
@@ -0,0 +1,12 @@
+import 'package:hello_world/repository/tmdb_api_client.dart';
+import 'package:hello_world/models/Movie.dart';
+
+class TMDBRepository{
+  final TMDBApiClient tmdbApiClient;
+  TMDBRepository({this.tmdbApiClient});
+
+  Future<List<Movie>> fetchMovies({int page}) async {
+    return await tmdbApiClient.fetchMovies(page: page);
+  }
+
+}
