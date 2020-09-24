315bc31865b11092d3291b011b2a63bcc13cb8a9
lucas-rss <52760079+lucas-rss@users.noreply.github.com>
2020-09-23 22:29:48 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:hello_world/bloc/movies_bloc.dart';
 import 'package:hello_world/repository/tmdb_api_client.dart';
 import 'package:hello_world/repository/tmdb_repository.dart';
+import 'package:hello_world/views/home_page.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -22,7 +25,6 @@ class MyApp extends StatelessWidget {
       tmdbApiClient: TMDBApiClient(
         httpClient: http.Client(),
       ),
-
     );
   
   @override
@@ -32,65 +34,18 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
        
-
         primarySwatch: Colors.blue,
         
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(
-        title: 'Isso aqui é Flamengo!'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-       
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: BlocProvider(
+        create: (context) => MovieBloc(tmdbRepository: tmdbRepository)
+        ..add(
+          FetchMovies(),
         ),
+
+        child: HomePage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
